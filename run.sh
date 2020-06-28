38a16ccfd750fd0afd8d773715073b444c8643c8
Mike Redko <30830926+somerandomdev49@users.noreply.github.com>
2020-06-28 20:52:52 +0300

    hmm

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n -------------------- [BUILD] --------------------"
+printf "\n\n\n\n\n\n\n\n\n\n\n\n\n -------------------- [BUILD] --------------------"
 #export SFML="/Users/Mishka/Desktop/SFML-2.5.1"
 
 g++ -c main.cpp -std=c++17 -I"$SFML/include"
 g++ main.o -std=c++17 -Wcomment -o main -L"$SFML/lib" -lsfml-graphics -lsfml-window -lsfml-system
-echo "\n\n -------------------- [RUN] -------------------- "
+printf "\n\n -------------------- [RUN] -------------------- "
 # echo $1 : $2 : $3 : $4
 export LD_LIBRARY_PATH="$SFML/lib" && export UBSAN_OPTIONS=print_stacktrace=1 && ./main $1
 # export LD_LIBRARY_PATH="$SFML/lib" && leaks -atExit -- ./main $1 | grep LEAK: # 
